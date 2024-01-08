361342bd983db093829c7b5741b273d623dbe5a6
Miaoday <Starryvero@gmail.com>
2024-01-08 19:15:16 +0100

    update file name

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
 import Header from './layouts/header/Header.jsx';
+import Footer from './layouts/footer/Footer.jsx';
 import Home from './pages/home/Home.jsx';
 
 function App() {
@@ -13,6 +14,8 @@ function App() {
             <Route path="/" element={<Home />} />
           </Routes>      
         </div>
+        <div />
+        <footer/>
       </Router>
   );
 }
---
+++ src/layouts/footer/footer.jsx
@@ -0,0 +1,12 @@
+import Logo_footer from '../../assets/images/mobile_Footer.png';
+import './footer.scss';
+
+function Footer () {
+  return(
+    <footer>
+    <img src={Logo_footer} alt='logo-of-footer-mobile'/>
+    </footer>
+  )
+}
+
+export default Footer;
