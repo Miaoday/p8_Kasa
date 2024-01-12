98455a9afe7b633c1e1c5fd93130e6c87eb7308f
Miaoday <Starryvero@gmail.com>
2024-01-12 15:28:47 +0100

    collection update

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import '../card/card.scss';
 import { Link } from 'react-router-dom';
+import '../card/card.scss';
 
 function Card ({id, cover, title}) {
   console.log(id,cover,title);
   return(
   <Link to={`/appartements/${id}`} >
-  
     <div className="card">
-    <img src={cover} alt='cover-of-appartement' className="card_img"/>
-    <h3 className="card_txt">{title}</h3>
+      <img src={cover} alt='cover of the appartement' className="card_img"/>
+      <h3 className="card_txt">{title}</h3>
     </div>
   </Link>
   )
--- src/components/collection/Collection.jsx
+++ src/components/collection/Collection.jsx
@@ -1,20 +1,19 @@
 // import React from 'react';
 import Card from '../card/Card.jsx';
-import '../collection/collection.scss';
 import dataList from '../data/appartements.json';
+import '../collection/collection.scss';
 
 function Collection () {
-
   return(
     <section>
       {dataList.map(function(data){
         return(
-        <Card 
-        key={data.id} 
-        id={data.id} 
-        title={data.title} 
-        cover={data.cover} 
-        />
+          <Card
+          key={data.id} 
+          id={data.id} 
+          title={data.title} 
+          cover={data.cover} 
+          />
         )
       })}
     </section>
