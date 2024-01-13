6b6f8788af24c8178b4570ad04ae919cd997c6f5
Miaoday <Starryvero@gmail.com>
2024-01-13 23:15:20 +0100

    error name

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Banner from "../../components/banner/Bannerr";
+import Banner from "../../components/banner/Banner";
 import './about.scss';
 import bannerAbout from "../../assets/images/banner_about.png"
 
