9e85cbe8b011c9afe90feb329e8d0ae1dc118470
Miaoday <Starryvero@gmail.com>
2024-01-17 11:18:57 +0100

    error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
 import Header from './layouts/header/Header.jsx';
-import Footer from './layouts/footer/Footer.jsx';
+import Footer from './layouts/footer/Footerr.jsx';
 import Home from './pages/home/Home.jsx';
 import About from './pages/about/About.jsx';
 import Error from './pages/error/Error.jsx';
