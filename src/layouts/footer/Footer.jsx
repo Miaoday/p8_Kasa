5fcc9f4211b2e0ed4537af7ec90545a34e09eac6
Miaoday <Starryvero@gmail.com>
2024-01-17 12:05:01 +0100

    error RD

--- src/layouts/footer/Footer.jsx
+++ src/layouts/footer/Footer.jsx
@@ -1,10 +1,13 @@
-import LogoFooter from '../../assets/images/footer_mobile.png';
+import React from 'react';
+import FooterMobile from '../../assets/images/footer_mobile.png';
+import FooterDesktop from '../../assets/images/footer_desktop.png';
 import './footer.scss';
 
 function Footer () {
+  const isMobile = window.innerWidth <= 767;
   return(
     <footer>
-    <img src={LogoFooter} alt='logo-of-footer-mobile'/>
+    <img src={isMobile ? FooterMobile : FooterDesktop} alt='footer-logo'/>
     </footer>
   )
 }
