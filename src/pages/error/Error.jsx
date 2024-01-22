ff5123211590409e2e9b2780e6f18bcef047890e
Miaoday <Starryvero@gmail.com>
2024-01-22 13:34:31 +0100

    error page

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,7 +1,13 @@
 import './error.scss';
 
 function Error () {
+  <div>
+    <div>404</div>
   
+    <div>Oups! La page que vous demandez n'existe pas.</div>
+
+    <div>Retourner sur la page d’accueil</div>
+  </div>
 
 }
 
